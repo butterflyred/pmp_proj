bdec43c8cde03ec01287dcd7e860cb8bdb72cd17
NCKU-CAV <nckueecav@gmail.com>
2020-09-19 16:52:56 +0800

    Edit memory length

--- rtl/PMP.sv
+++ rtl/PMP.sv
@@ -8,16 +8,16 @@ typedef struct packed {
   } pmpcfg_t;
 
 module PMP #(
-    parameter XLEN = 32,
-    parameter PLEN = 34,
+    parameter VLEN = xx,
+    parameter PLEN = xx,
     parameter PMP_CNT = 16
 )
 (
     input   pmpcfg_t    [PMP_CNT-1:0]          io_pmpcfg,
-    input               [PMP_CNT-1:0][31:0]    io_pmpaddr,
+    input               [PMP_CNT-1:0][VLEN:0]  io_pmpaddr,
     input                            [1:0]     io_prv,
     input                                      io_req,
-    input                            [33:0]    io_addr, // physical memory address
+    input                            [PLEN:0]  io_addr,
     input                            [1:0]     io_size,
     input                                      io_r,
     input                                      io_w,
